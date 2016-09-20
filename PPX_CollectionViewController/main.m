d3c2d5427656f93392858f4d1ca6baf6beddb3b8
jiboxue <jibx@ichsy.com>
2016-09-20 15:19:13 +0800

    Initial Commit

---
+++ PPX_CollectionViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PPX_CollectionViewController
+//
+//  Created by pipixia on 16/9/20.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ PPX_CollectionViewControllerTests/PPX_CollectionViewControllerTests.m
@@ -0,0 +1,39 @@
+//
+//  PPX_CollectionViewControllerTests.m
+//  PPX_CollectionViewControllerTests
+//
+//  Created by pipixia on 16/9/20.
+//  Copyright © 2016年 pipixia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PPX_CollectionViewControllerTests : XCTestCase
+
+@end
+
+@implementation PPX_CollectionViewControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
